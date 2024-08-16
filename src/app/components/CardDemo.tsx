19459b00c1f78d61e9087391bde7ad640d6f84fd
Roger <codinaroger2@gmail.com>
2024-08-16 15:27:05 +0200

    added start button and fixed font size

--- src/app/components/CardDemo.tsx
+++ src/app/components/CardDemo.tsx
@@ -30,6 +30,7 @@ function shuffleArray(array: any[]) {
 }
 
 export function CardDemo({ className, ...props }: CardProps) {
+  const [hasStarted, setHasStarted] = useState(false); // State to track if the quiz has started
   const [shuffledQuestions, setShuffledQuestions] = useState(() =>
     shuffleArray(originalQuestions),
   ); // State to hold shuffled questions
@@ -84,100 +85,124 @@ export function CardDemo({ className, ...props }: CardProps) {
     );
   };
 
+  const handleStartQuiz = () => {
+    setHasStarted(true); // Set the state to true when the start button is clicked
+  };
+
   return (
-    <Card className={cn(className)} {...props}>
-      <CardHeader>
-        <CardTitle>Quiz Question</CardTitle>
-        <CardDescription>
-          {currentQuestion.multiSelect ? "Multiple Answers" : "Single Answer"}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div>
-          <p className="text-sm font-medium leading-none">
-            {currentQuestion.question}
-          </p>
-        </div>
-        <div className="grid items-start gap-4">
-          {currentQuestion.options.map((option, idx) => (
-            <div key={idx}>
-              <input
-                type={currentQuestion.multiSelect ? "checkbox" : "radio"}
-                value={option}
-                id={`option-${idx}`}
-                checked={selectedOptions.includes(option)}
-                onChange={() => handleOptionChange(option)}
-                className="peer sr-only"
-                disabled={showAnswer} // Disable the input if answer is shown
-              />
-              <Label
-                htmlFor={`option-${idx}`}
-                className={cn(
-                  "border-muted bg-popover hover:bg-accent hover:text-accent-foreground flex flex-col items-center justify-between rounded-md border-2 p-4 peer-checked:border-slate-400",
-                  {
-                    "cursor-not-allowed opacity-50": showAnswer, // Make the options visually disabled if answer is shown
-                  },
-                )}
-              >
-                {option}
-              </Label>
-            </div>
-          ))}
-        </div>
-        {showAnswer && (
-          <div className="mt-4 space-y-2">
-            {isCorrect !== null && (
-              <Alert variant={isCorrect ? "default" : "destructive"}>
-                {isCorrect ? (
-                  <CheckCircle className="h-4 w-4" />
-                ) : (
-                  <AlertCircle className="h-4 w-4" />
-                )}
-                <AlertTitle>
-                  {isCorrect ? "Congratulations!" : "Oops!"}
-                </AlertTitle>
-                <AlertDescription>
-                  {isCorrect
-                    ? "You've selected the correct answer."
-                    : "The answer you selected is incorrect."}
-                </AlertDescription>
-              </Alert>
-            )}
-            {!isCorrect && (
-              <>
-                <p className="text-sm font-medium leading-none">
-                  Correct Answer:
-                </p>
-                <p className="text-muted-foreground text-sm">
-                  {Array.isArray(currentQuestion.answer)
-                    ? currentQuestion.answer.join(", ")
-                    : currentQuestion.answer}
-                </p>
-              </>
-            )}
-            <p className="text-sm font-medium leading-none">Explanation:</p>
-            <p className="text-muted-foreground text-sm">
-              {currentQuestion.explanation}
-            </p>
+    <>
+      <h1
+        className={`${hasStarted ? "text-3xl" : "text-5xl sm:text-[5rem]"} text-center font-extrabold tracking-tight text-black`}
+      >
+        AWS <span className="text-indigo-500">Cloud Practitioner</span>{" "}
+        Certification Prep
+      </h1>
+      <Card className={cn(className, "max-w-3xl")} {...props}>
+        {!hasStarted ? (
+          <div className="flex items-center justify-center">
+            <Button onClick={handleStartQuiz}>Start Quiz</Button>
           </div>
+        ) : (
+          <>
+            <CardHeader>
+              <CardTitle>Quiz Question</CardTitle>
+              <CardDescription>
+                {currentQuestion.multiSelect
+                  ? "Multiple Answers"
+                  : "Single Answer"}
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div>
+                <p className="text-sm font-medium leading-none">
+                  {currentQuestion.question}
+                </p>
+              </div>
+              <div className="grid items-start gap-4">
+                {currentQuestion.options.map((option, idx) => (
+                  <div key={idx}>
+                    <input
+                      type={currentQuestion.multiSelect ? "checkbox" : "radio"}
+                      value={option}
+                      id={`option-${idx}`}
+                      checked={selectedOptions.includes(option)}
+                      onChange={() => handleOptionChange(option)}
+                      className="peer sr-only"
+                      disabled={showAnswer} // Disable the input if answer is shown
+                    />
+                    <Label
+                      htmlFor={`option-${idx}`}
+                      className={cn(
+                        "border-muted bg-popover hover:bg-accent hover:text-accent-foreground flex flex-col items-center justify-between rounded-md border-2 p-4 peer-checked:border-slate-400",
+                        {
+                          "cursor-not-allowed opacity-50": showAnswer, // Make the options visually disabled if answer is shown
+                        },
+                      )}
+                    >
+                      {option}
+                    </Label>
+                  </div>
+                ))}
+              </div>
+              {showAnswer && (
+                <div className="mt-4 space-y-2">
+                  {isCorrect !== null && (
+                    <Alert variant={isCorrect ? "default" : "destructive"}>
+                      {isCorrect ? (
+                        <CheckCircle className="h-4 w-4" />
+                      ) : (
+                        <AlertCircle className="h-4 w-4" />
+                      )}
+                      <AlertTitle>
+                        {isCorrect ? "Congratulations!" : "Oops!"}
+                      </AlertTitle>
+                      <AlertDescription>
+                        {isCorrect
+                          ? "You've selected the correct answer."
+                          : "The answer you selected is incorrect."}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {!isCorrect && (
+                    <>
+                      <p className="text-sm font-medium leading-none">
+                        Correct Answer:
+                      </p>
+                      <p className="text-muted-foreground text-sm">
+                        {Array.isArray(currentQuestion.answer)
+                          ? currentQuestion.answer.join(", ")
+                          : currentQuestion.answer}
+                      </p>
+                    </>
+                  )}
+                  <p className="text-sm font-medium leading-none">
+                    Explanation:
+                  </p>
+                  <p className="text-muted-foreground text-sm">
+                    {currentQuestion.explanation}
+                  </p>
+                </div>
+              )}
+            </CardContent>
+            <CardFooter>
+              <Button
+                className="w-full"
+                onClick={showAnswer ? handleNextQuestion : handleCheckAnswer}
+              >
+                {showAnswer ? (
+                  <>
+                    Next <ArrowRight className="ml-2 h-4 w-4" />
+                  </>
+                ) : (
+                  <>
+                    <Check className="mr-2 h-4 w-4" /> Check Answer
+                  </>
+                )}
+              </Button>
+            </CardFooter>
+          </>
         )}
-      </CardContent>
-      <CardFooter>
-        <Button
-          className="w-full"
-          onClick={showAnswer ? handleNextQuestion : handleCheckAnswer}
-        >
-          {showAnswer ? (
-            <>
-              Next <ArrowRight className="ml-2 h-4 w-4" />
-            </>
-          ) : (
-            <>
-              <Check className="mr-2 h-4 w-4" /> Check Answer
-            </>
-          )}
-        </Button>
-      </CardFooter>
-    </Card>
+      </Card>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-center text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
-          AWS <span className="text-indigo-500">Cloud Practitioner</span>{" "}
-          Certification Prep
-        </h1>
-        <div className="max-w-3xl">
-          <CardDemo />
-        </div>
+        <CardDemo />
       </div>
     </main>
   );
